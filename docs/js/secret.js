046424b916ccdaeee041e341d5ce56c4a7c0a62f
trashbird <chrissy123376@gmail.com>
2023-11-09 15:39:47 -0500

    fix window link 2?

--- docs/js/secret.js
+++ docs/js/secret.js
@@ -19,6 +19,7 @@ function secretDouble(){
     if(interact2){
         return ;
     }else{
+        //this should work
         stopPlease = window.open("/secret/you-need-to-stop.html", "", "width=500, height=600");
         stopPlease.addEventListener("load", (event) => {
             var ranMoveTime = 5;
